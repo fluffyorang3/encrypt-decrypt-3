ff2e62999951c09201f058c5fafb33472f23b0b5
fluffyorang3 <fluffyorang3@gmail.com>
2024-06-04 01:13:12 +0200

    Complete encrypt/decrypt tool

--- index.js
+++ index.js
@@ -1,18 +1,72 @@
-const characters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890.,?! "
+//This program will take a string, encrypt it, then decrypt it
 
-const array = characters.split("")
+let string = "Hey this is a message";
 
-console.log(array)
+let mapping = {};
 
-let emptyArray = [];
 
-for (i = 0; i < 67; i++) {
-  for(m = 0; m <67; m++) {
-    emptyArray.push(`${array[i]}${array[m]}`)
+
+const map1 = "qwertyu iopasdfghjklzxcvbnm1234567890"
+const map2 = "6acbdkef9ghi0j2lno 1x3pqr5stm7u8v4wyz"
+
+const map1Split = map1.split("");
+const map2Split = map2.split("");
+
+function mapToMapping() {
+  for(let i = 0; i < map1Split.length; i++) {
+    mapping[map1[i]] = map2[i];
   }
 }
 
-console.log(emptyArray)
+mapToMapping();
+
+console.log(mapping);
+console.log(map1Split, map2Split);
+
+
+const messageInput = document.querySelector("input");
+
+function encrypt(encryptedString) {
+  encryptedString = text = messageInput.value.toLowerCase().split("").map((value) => {
+    return mapping[(value)];
+  })
+  return encryptedString.join("");
+}
+
+function decrypt(decryptedString) {
+  decryptedString = encrypt().split("").map((value) => {
+    return Object.keys(mapping).find(key => mapping[key] === value);
+  })
+  return decryptedString.join("")
+}
+
+console.log(encrypt())
+console.log(decrypt())
+
+
+const button = document.querySelector("button");
+
+const messageField = document.querySelector(".message")
+
+const encryptedField = document.querySelector(".encrypted")
+
+const decryptedField = document.querySelector(".decrypted")
+
+button.addEventListener("click", (message,encryptt,decryptt) => {
+  message = encryptedField.value
+  encryptt = encryptedField.value
+  decryptt = decryptedField.value
+
+  encryptedField.value = encrypt(message)
+  decryptedField.value = decrypt(encryptedField.value);
+  console.log(encryptt)
+  console.log(decryptt)
+})
+
+
+
+
+
 
 
 
